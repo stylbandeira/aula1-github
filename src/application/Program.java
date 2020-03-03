89fb4df7727bff9917bcea00e71a87003b810388
Styl Bandeira <stylbandeira@hotmail.com>
2020-03-02 22:15:39 -0300

    Adicionado tudo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+import entities.entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Bom dia!");
-        System.out.println("Boa tarde!");
-        System.out.println("Boa noite!");
+        Order order = new Order(1080,new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
     }
 }
